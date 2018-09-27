0045fd4af76bb2bad95a446b445511d8b3a40326
Luciano Milen <lucianomilen@dcc.ufmg.br>
2018-09-27 19:01:17 -0300

    simple header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Header.scss';
+
+export default class Header extends React.Component {
+    public render(){
+        return (
+            <div className={'header'}>
+                IEX Dashboard
+            </div>
+        )
+    }
+}
